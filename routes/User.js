b0d03723d121bf0da34625fdd8dd652e23010dfc
Iso-Doss <dossou.israel48@gmail.com>
2022-09-08 17:13:16 +0100

    Update user routes - add middleware

--- routes/User.js
+++ routes/User.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const userController = require("../controllers/User");
 
+const signupLoginMiddleware = require("../middleware/SignupLogin");
+
 const router = express.Router();
 
-router.post('/signup', userController.signup);
+router.post('/signup', signupLoginMiddleware, userController.signup);
 
-router.post('/login', userController.login);
+router.post('/login', signupLoginMiddleware, userController.login);
 
 module.exports = router;
