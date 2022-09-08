eff62bc96f40fa6d37503f71b526a28bceb762c7
Iso-Doss <dossou.israel48@gmail.com>
2022-09-08 14:52:44 +0100

    Setting up the basic User controller structure

---
+++ controllers/Suace.js
@@ -0,0 +1,56 @@
+const Sauce = require("../models/Sauce");
+
+/**
+ * Create sauce.
+ *
+ * @param req The request
+ * @param res The response
+ * @param next The next.
+ */
+exports.createSauce = (req, res, next) => {
+
+};
+
+/**
+ * Read a sauce.
+ *
+ * @param req The request
+ * @param res The response
+ * @param next The next.
+ */
+exports.readSauce = (req, res, next) => {
+
+};
+
+/**
+ * Read all sauces.
+ *
+ * @param req The request
+ * @param res The response
+ * @param next The next.
+ */
+exports.readSauces = (req, res, next) => {
+
+};
+
+/**
+ * Update a sauce.
+ *
+ * @param req The request
+ * @param res The response
+ * @param next The next.
+ */
+exports.updateSauce = (req, res, next) => {
+
+};
+
+/**
+ * Delete a sauce.
+ *
+ * @param req The request
+ * @param res The response
+ * @param next The next.
+ */
+exports.deleteSauce = (req, res, next) => {
+
+};
