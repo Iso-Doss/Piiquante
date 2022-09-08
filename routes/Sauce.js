b5d844371cb75654fdb28d62d0c22819ed0a0941
Iso-Doss <dossou.israel48@gmail.com>
2022-09-08 15:01:17 +0100

    Register sauce routes

--- routes/Sauce.js
+++ routes/Sauce.js
@@ -1,5 +1,17 @@
 const express = require('express');
 
+const sauceController = require("../controllers/Suace");
+
 const router = express.Router();
+
+router.post('/', sauceController.createSauce);
+
+router.get('/:id', sauceController.readSauce);
+
+router.get('/', sauceController.readSauces);
+
+router.put('/:id', sauceController.updateSauce);
+
+router.delete('/:id', sauceController.deleteSauce);
 
 module.exports = router;
