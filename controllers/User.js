82e1e9dbb76fbc6ab2f28f4ea38042ef4092b884
Iso-Doss <dossou.israel48@gmail.com>
2022-09-08 14:52:27 +0100

    Setting up the basic User controller structure

---
+++ controllers/User.js
@@ -0,0 +1,13 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+
+exports.signup = (req, res, next) => {
+
+};
+
+exports.login = (req, res, next) => {
+
+};
