0b6359bd301d77f2de95cc47e084b57b11c6264f
Iso-Doss <dossou.israel48@gmail.com>
2022-09-08 13:35:54 +0100

    Add middleware in application to fix CORS issue

--- App.js
+++ App.js
@@ -22,4 +22,12 @@ app.use(express.json());
 // Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
 app.use(helmet());
 
+// Fix CORS issue.
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 module.exports = app;
